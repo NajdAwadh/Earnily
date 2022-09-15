2aae1e908aee183a6e7377b38cd5d8b77b5e2df3
Raghad <rk.2002.kr@gmail.com>
2022-09-15 03:45:09 +0300

    remove debug logo

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       // home: HomeScreen(),
       home: MainPage(),
       // home: MainTask(),
