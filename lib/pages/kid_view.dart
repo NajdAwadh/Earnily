bf329480d333f17d057f4bdb6e1949ab69d4dda4
najdqm <100093366+najdqm@users.noreply.github.com>
2022-10-09 01:17:29 +0300

    Update kid_view.dart

---
+++ lib/pages/kid_view.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class kidView extends StatefulWidget {
+  const kidView({super.key});
+
+  @override
+  State<kidView> createState() => _kidViewState();
+}
+
+class _kidViewState extends State<kidView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.amber,
+      ),
+    );
+  }
+}
