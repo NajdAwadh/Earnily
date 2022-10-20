aaa46cbeb14bd4ba055f8049c7eae54dec165e80
Raghad <rk.2002.kr@gmail.com>
2022-10-20 04:55:56 +0300

    calendar

---
+++ lib/calendar/calendar.dart
@@ -0,0 +1,162 @@
+import 'package:earnily/screen/profile_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:googleapis/driveactivity/v2.dart';
+import 'package:provider/provider.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+import '../api/taskApi.dart';
+import '../models/task.dart';
+import '../notifier/taskNotifier.dart';
+import 'event.dart';
+import 'package:intl/intl.dart';
+
+class Calendar extends StatefulWidget {
+  @override
+  _CalendarState createState() => _CalendarState();
+}
+
+class _CalendarState extends State<Calendar> {
+  late Map<DateTime, List<Event>> selectedEvents;
+  CalendarFormat format = CalendarFormat.week;
+  DateTime selectedDay = DateTime.now();
+  DateTime focusedDay = DateTime.now();
+  final formatter = DateFormat('yMd');
+
+  TextEditingController _eventController = TextEditingController();
+
+  @override
+  void initState() {
+    selectedEvents = {};
+    TaskNotifier taskNotifier =
+        Provider.of<TaskNotifier>(context, listen: false);
+
+    getTask(taskNotifier);
+    super.initState();
+  }
+
+  List<Event> _getEventsfromDay(DateTime date) {
+    return selectedEvents[date] ?? [];
+  }
+
+  void addToCalendar(String name, DateTime date) {
+    if (selectedDay == date) {
+      if (selectedEvents[selectedDay] != null) {
+        print('in if');
+        selectedEvents[selectedDay]?.add(
+          Event(title: name),
+        );
+      } else {
+        print('else');
+        selectedEvents[selectedDay] = [Event(title: name)];
+      }
+    }
+  }
+
+  void add(List<Task> tasks) {
+    for (var i = 0; i < tasks.length; i++) {
+      String name = tasks[i].taskName;
+      DateTime date = formatter.parseUTC(tasks[i].date);
+      _getEventsfromDay(date);
+      addToCalendar(name, date);
+    }
+  }
+
+  @override
+  void dispose() {
+    _eventController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    TaskNotifier taskNotifier =
+        Provider.of<TaskNotifier>(context, listen: false);
+    List<Task> task = taskNotifier.taskList;
+
+    add(task);
+
+    return Scaffold(
+      body: Column(
+        children: [
+          TableCalendar(
+            locale: 'en_US',
+            focusedDay: selectedDay,
+            firstDay: DateTime(1990),
+            lastDay: DateTime(2050),
+            calendarFormat: format,
+            onFormatChanged: (CalendarFormat _format) {
+              setState(() {
+                format = _format;
+              });
+            },
+            startingDayOfWeek: StartingDayOfWeek.sunday,
+            daysOfWeekVisible: true,
+
+            //Day Changed
+            onDaySelected: (DateTime selectDay, DateTime focusDay) {
+              setState(() {
+                selectedDay = selectDay;
+                focusedDay = focusDay;
+                selectedEvents.clear();
+              });
+              print(focusedDay);
+            },
+            selectedDayPredicate: (DateTime date) {
+              return isSameDay(selectedDay, date);
+            },
+
+            eventLoader: _getEventsfromDay,
+
+            //To style the Calendar
+            calendarStyle: CalendarStyle(
+              isTodayHighlighted: true,
+              selectedDecoration: BoxDecoration(
+                color: Colors.black,
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(5.0),
+              ),
+              selectedTextStyle: TextStyle(color: Colors.white),
+              todayDecoration: BoxDecoration(
+                color: Colors.deepPurple,
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(5.0),
+              ),
+              defaultDecoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(5.0),
+              ),
+              weekendDecoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(5.0),
+              ),
+              markerDecoration: BoxDecoration(
+                color: Colors.blue,
+                shape: BoxShape.circle,
+              ),
+            ),
+
+            headerStyle: HeaderStyle(
+              formatButtonVisible: true,
+              titleCentered: true,
+              formatButtonShowsNext: true,
+              formatButtonDecoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.circular(5.0),
+              ),
+              formatButtonTextStyle: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+          ..._getEventsfromDay(selectedDay).map(
+            (Event event) => ListTile(
+              title: Text(
+                event.title,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
