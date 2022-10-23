fc59b67862ab6dee50494d38a4627c2f1f1aefc6
LamisQa <lamisq7@outlook.com>
2022-10-23 08:30:41 +0300

    Update profile

--- lib/screen/profile_screen.dart
+++ lib/screen/profile_screen.dart
@@ -27,6 +27,8 @@ String name = '';
 String image = '';
 String email = '';
 String family = '';
+GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
 class _ProfileScreenState extends State<ProfileScreen> {
   TextEditingController nameController = TextEditingController();
   TextEditingController _familyController = TextEditingController();
@@ -40,6 +42,9 @@ class _ProfileScreenState extends State<ProfileScreen> {
     print(email);
     _getUserDetail();
   }
+
+  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -70,174 +75,184 @@ class _ProfileScreenState extends State<ProfileScreen> {
           ),
         ),
       ),
-      body: LoadingOverlay(
-        isLoading: isLoading,
-        progressIndicator: ProcessingWidget(),
-        child: Scaffold(
-            appBar: AppBar(
-              backgroundColor: Colors.transparent,
-              elevation: 0,
-              //  iconTheme: IconThemeData(color: Colors.black),
-            ),
-            backgroundColor: Colors.white,
-            body: SingleChildScrollView(
-              child: Padding(
-                  padding: EdgeInsets.fromLTRB(40, 0, 40, 0),
-                child: Column(
-                  children: [
-                 
-              SizedBox(
-                      height: 40,
-                    ),
-             
-                    SizedBox(
-                      height: 20,
-                    ),
-                    // GestureDetector(
-                    //   onTap: (){
-                    //     isEnabled =! isEnabled;
-                    //   setState(() {});
-                    //     },
-                    //   child: Align(
-                    //       alignment: Alignment.centerRight,
-                    //       child: Text('تعديل',style: TextStyle(fontSize: 16.0,fontWeight: FontWeight.w600,color: Colors.black),)),
-            
-                    // ),
-            
-                    SizedBox(
-                      height: 20,
-                    ),
-                    NewText(
-                      text: ':الاسم الاول',
-                      fontWeight: FontWeight.bold,
-                      size: 18,
-                    ),
-              
-                     Container(
-                        alignment: Alignment.topRight,
-                        height: 50,
-                        width: MediaQuery.of(context).size.width,
-                        decoration: BoxDecoration(
-                            color: Colors.grey[100],
-                            borderRadius: BorderRadius.circular(15)),
-                        child: TextFormField(
-                          controller: nameController,
-                           enabled: isEnabled,
-                          textAlign: TextAlign.right,
-                          decoration: InputDecoration(
-                              border: InputBorder.none,
-                              hintText: name,
-                              hintTextDirection: ui.TextDirection.rtl,
-                              hintStyle: TextStyle(
-                                color: Colors.black,
-                                fontSize: 17,
-                              ),
-                              contentPadding: EdgeInsets.only(
-                                left: 20,
-                                right: 20,
-                              )),
-                        ),
-                      ),    
-                    SizedBox(
-                      height: 10,
-                    ),
-                    NewText(
-                      text: ':العائلة',
-                      fontWeight: FontWeight.bold,
-                      size: 18,
-                    ),
-      
-                           Container(
-                        alignment: Alignment.topRight,
-                        height: 50,
-                        width: MediaQuery.of(context).size.width,
-                        decoration: BoxDecoration(
-                            color: Colors.grey[100],
-                            borderRadius: BorderRadius.circular(15)),
-                        child: TextFormField(
-                          controller: _familyController,
-                           enabled: isEnabled,
-                          textAlign: TextAlign.right,
-                          decoration: InputDecoration(
-                              border: InputBorder.none,
-                              hintText: family,
-                              hintTextDirection: ui.TextDirection.rtl,
-                              hintStyle: TextStyle(
-                                color: Colors.black,
-                                fontSize: 17,
-                              ),
-                              contentPadding: EdgeInsets.only(
-                                left: 20,
-                                right: 20,
-                              )),
-                        ),
-                      ), 
-                    SizedBox(
-                      height: 10,
-                    ),
-                    NewText(
-                      text: ':البريد الإلكتروني',
-                      fontWeight: FontWeight.bold,
-                      size: 18,
-                    ),
-                
-                        Container(
-                        alignment: Alignment.topRight,
-                        height: 50,
-                        width: MediaQuery.of(context).size.width,
-                        decoration: BoxDecoration(
-                            color: Colors.grey[100],
-                            borderRadius: BorderRadius.circular(15)),
-                        child: TextFormField(
-                          controller: _emailController,
-                           enabled: isEnabled,
-                          textAlign: TextAlign.right,
-                          decoration: InputDecoration(
-                              border: InputBorder.none,
-                              hintText: email,
-                              hintTextDirection: ui.TextDirection.rtl,
-                              hintStyle: TextStyle(
-                                color: Colors.black,
-                                fontSize: 17,
-                              ),
-                              contentPadding: EdgeInsets.only(
-                                left: 20,
-                                right: 20,
-                              )),
-                        ),
-                      ), 
-                         SizedBox(
-                      height: 25,
-                    ),
-                    if (isEnabled == false)
-                      NewButton(
-                          text: 'تعديل',
-                          height: 100,
-                          width: 320,
-                          onClick: () {
-                            isEnabled = !isEnabled;
-                            setState(() {});
-                          }),
-                    if (isEnabled == true)
-                      NewButton(
-                          height: 100,
-                          width: 320,
-                          text: 'حفظ التغييرات',
-                          onClick: () {
-                            resetEmail(_emailController.text);
-                            updateProfile();
-                          }),
-                    if (isEnabled == true)
-                      NewButton(
-                          height: 100,
-                          width: 320,
-                          text: 'إلغاء',
-                          onClick: () {
-                               Navigator.of(context).pop();  })
-                  ],
-                ),
+      body: Form(
+        key: formKey,
+        child: LoadingOverlay(
+          isLoading: isLoading,
+          progressIndicator: ProcessingWidget(),
+          child: Scaffold(
+              appBar: AppBar(
+                backgroundColor: Colors.transparent,
+                elevation: 0,
+                //  iconTheme: IconThemeData(color: Colors.black),
               ),
-            )),
+              backgroundColor: Colors.white,
+              body: SingleChildScrollView(
+                child: Padding(
+                    padding: EdgeInsets.fromLTRB(40, 0, 40, 0),
+                  child: Column(
+                    children: [
+                   
+                SizedBox(
+                        height: 40,
+                      ),
+               
+                      SizedBox(
+                        height: 20,
+                      ),
+                      // GestureDetector(
+                      //   onTap: (){
+                      //     isEnabled =! isEnabled;
+                      //   setState(() {});
+                      //     },
+                      //   child: Align(
+                      //       alignment: Alignment.centerRight,
+                      //       child: Text('تعديل',style: TextStyle(fontSize: 16.0,fontWeight: FontWeight.w600,color: Colors.black),)),
+              
+                      // ),
+              
+                      SizedBox(
+                        height: 20,
+                      ),
+
+                      NewText(
+                        text: ':الاسم الاول',
+                        fontWeight: FontWeight.bold,
+                        size: 18,
+                      ),
+                
+                       Container(
+                          alignment: Alignment.topRight,
+                          height: 50,
+                          width: MediaQuery.of(context).size.width,
+                          decoration: BoxDecoration(
+                              color: Colors.grey[100],
+                              borderRadius: BorderRadius.circular(15)),
+                          child: TextFormField(
+                            validator: (val) =>
+                              val!.isEmpty ? 'أدخل الاسم  ' : null,
+                            controller: nameController,
+                             enabled: isEnabled,
+                            textAlign: TextAlign.right,
+                            decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: name,
+                                hintTextDirection: ui.TextDirection.rtl,
+                                hintStyle: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 17,
+                                ),
+                                contentPadding: EdgeInsets.only(
+                                  left: 20,
+                                  right: 20,
+                                )),
+                          ),
+                        ),    
+                      SizedBox(
+                        height: 10,
+                      ),
+                      NewText(
+                        text: ':العائلة',
+                        fontWeight: FontWeight.bold,
+                        size: 18,
+                      ),
+        
+                             Container(
+                          alignment: Alignment.topRight,
+                          height: 50,
+                          width: MediaQuery.of(context).size.width,
+                          decoration: BoxDecoration(
+                              color: Colors.grey[100],
+                              borderRadius: BorderRadius.circular(15)),
+                          child: TextFormField(
+                            validator: (val) =>
+                              val!.isEmpty ? 'أدخل اسم العائلة  ' : null,
+                            controller: _familyController,
+                             enabled: isEnabled,
+                            textAlign: TextAlign.right,
+                            decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: family,
+                                hintTextDirection: ui.TextDirection.rtl,
+                                hintStyle: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 17,
+                                ),
+                                contentPadding: EdgeInsets.only(
+                                  left: 20,
+                                  right: 20,
+                                )),
+                          ),
+                        ), 
+                      SizedBox(
+                        height: 10,
+                      ),
+                      NewText(
+                        text: ':البريد الإلكتروني',
+                        fontWeight: FontWeight.bold,
+                        size: 18,
+                      ),
+                  
+                          Container(
+                          alignment: Alignment.topRight,
+                          height: 50,
+                          width: MediaQuery.of(context).size.width,
+                          decoration: BoxDecoration(
+                              color: Colors.grey[100],
+                              borderRadius: BorderRadius.circular(15)),
+                          child: TextFormField(
+                            validator: (val) =>
+                              val!.isEmpty ? 'أدخل البريد الاكتروني  ' : null,
+                            controller: _emailController,
+                             enabled: isEnabled,
+                            textAlign: TextAlign.right,
+                            decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: email,
+                                hintTextDirection: ui.TextDirection.rtl,
+                                hintStyle: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 17,
+                                ),
+                                contentPadding: EdgeInsets.only(
+                                  left: 20,
+                                  right: 20,
+                                )),
+                          ),
+                        ), 
+                           SizedBox(
+                        height: 25,
+                      ),
+                      if (isEnabled == false)
+                        NewButton(
+                            text: 'تعديل',
+                            height: 100,
+                            width: 320,
+                            onClick: () {
+                              isEnabled = !isEnabled;
+                              setState(() {});
+                            }),
+                      if (isEnabled == true)
+                        NewButton(
+                            height: 100,
+                            width: 320,
+                            text: 'حفظ التغييرات',
+                            onClick: () {
+                              resetEmail(_emailController.text);
+                              updateProfile();
+                            }),
+                      if (isEnabled == true)
+                        NewButton(
+                            height: 100,
+                            width: 320,
+                            text: 'إلغاء',
+                            onClick: () {
+                                 Navigator.of(context).pop();  })
+                    ],
+                  ),
+                ),
+              )),
+        ),
       ),
     );
   }
